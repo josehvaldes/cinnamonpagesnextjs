1865e183ca7102ff659b4f7cba97b0f7e68a1d8e
josehvaldes <jmurguia@gmail.com>
2026-06-29 17:33:33 -0400

    Add a genericRequest handler to centralize HTTP requests

--- lib/api/health.ts
+++ lib/api/health.ts
@@ -1,31 +1,6 @@
 import "server-only";
-
-export const API_BASE_URL =
-    process.env.API_BASE_URL ||
-    "https://localhost:44350/api";
-
-export const API_KEY = process.env.API_KEY || "your-api-key-here"; // Replace with your actual API key or use environment variables 
+import { getRequest } from "./genericRequest";
 
 export async function healthCheckStatus(): Promise<string> {
-
-    console.log(`Performing health check at: ${API_BASE_URL}/health`);
-    try
-    {
-        const response = await fetch(`${API_BASE_URL}/health`, {
-            headers: {
-                "x-api-key": API_KEY
-            }
-        });
-        if (!response.ok) {
-            console.error(`.Health check failed with status: ${response.status}`);
-            throw new Error(`Health check failed with status: ${response.status}`);
-        }
-        return response.text();
-    }
-    catch (error) {
-        console.error(`.Health check failed: ${error}`);
-        console.error(error);
-        throw error;
-    }
-    
+    return getRequest<string>("health", { excludeApiVersion: true });
 }
